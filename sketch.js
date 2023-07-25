deabd428b23051aa7ff729621d859be969ae7e1b
lxctoledo <140511307+lxctoledo@users.noreply.github.com>
2023-07-25 09:56:12 -0300

    upload arquivos

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let cor
+let posicaoHorizontal;
+let posicaoVertical;
+
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  cor= color(random(0, 255),random(0, 255), random(0, 255));
+  posicaoHorizontal= 200;
+  posicaoVertical= 200;
+  
+  
+}
+  
+function draw() {
+  
+
+  stroke("black")
+  fill(cor);
+  circle(posicaoHorizontal, posicaoVertical, 20);
+  
+  if(mouseX < posicaoHorizontal) {
+    posicaoHorizontal = posicaoHorizontal - 3;
+  }
+  if(mouseX > posicaoHorizontal) {
+    posicaoHorizontal = posicaoHorizontal + 3;
+  }
+  
+  if (mouseY < posicaoVertical) {
+    posicaoVertical--;
+  }
+   if (mouseY > posicaoVertical) {
+    posicaoVertical++;
+  }
+  
+  
+  
+  
+}
